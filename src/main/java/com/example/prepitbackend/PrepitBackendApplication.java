780d28808fa73bf189421acb25390ec4064233d3
ewald30 <berla.ewald30@gmail.com>
2022-03-24 01:34:50 +0200

    setup

---
+++ src/main/java/com/example/prepitbackend/PrepitBackendApplication.java
@@ -0,0 +1,13 @@
+package com.example.prepitbackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PrepitBackendApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(PrepitBackendApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/prepitbackend/PrepitBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.prepitbackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PrepitBackendApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
