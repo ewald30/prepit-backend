92ff0a550ffe36220d9bef195afe9f89bf8b2292
Berla Ewald <berlaewald@Berlas-MacBook-Pro-2.local>
2023-10-18 21:18:31 +0300

    fixed recipes.json not loading in .jar package

--- src/main/java/com/example/prepitbackend/service/dao/MealJSONRepoImpl.java
+++ src/main/java/com/example/prepitbackend/service/dao/MealJSONRepoImpl.java
@@ -2,6 +2,8 @@ package com.example.prepitbackend.service.dao;
 
 import java.io.FileReader;
 import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
 import java.util.ArrayList;
 
 import com.example.prepitbackend.domain.Meal;
@@ -14,23 +16,25 @@ import org.json.simple.parser.JSONParser;
 import org.json.simple.parser.ParseException;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.core.io.ClassPathResource;
 
 public class MealJSONRepoImpl implements MealJSONRepo {
 
     @Value("${file.recipe.path}")
-    String recipeFilePath;
+    private String recipeFilePath;
 
     @Override
     public JSONArray readAllJSON() {
         JSONParser parser = new JSONParser();
 
         try {
-            JSONArray jsonArray = (JSONArray) parser.parse(new FileReader(this.recipeFilePath));
+            // Use ClassPathResource to get the file as an InputStream
+            InputStream inputStream = new ClassPathResource(recipeFilePath).getInputStream();
+            JSONArray jsonArray = (JSONArray) parser.parse(new InputStreamReader(inputStream));
             return jsonArray;
         } catch (IOException | ParseException e) {
             e.printStackTrace();
         }
         return null;
     }
-
 }
